7b91e3dd72635dd4c8b2a81a236fecb6236589af
2293550437 <59769907+2293550437@users.noreply.github.com>
2020-01-13 16:57:26 +0800

    Update SpringbootThymeleafApplication.java

--- src/main/java/com/cheng/SpringbootThymeleafApplication.java
+++ src/main/java/com/cheng/SpringbootThymeleafApplication.java
@@ -2,11 +2,11 @@ package com.cheng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//修改或大货车
 @SpringBootApplication
 public class SpringbootThymeleafApplication {
     /**
-     * 新增注释********************************
+     * 新增注释***************牛皮的三方IC*****************
      * @param args
      */
     public static void main(String[] args) {
